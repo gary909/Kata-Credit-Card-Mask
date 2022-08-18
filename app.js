99c2f325ab011960eff32f32d95a5a8998468371
gary909 <garywhite61@hotmail.com>
2022-08-18 12:56:19 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+// return masked string
+function maskify(cc) {
+
+}
+
+console.log(maskify('4556364607935616')); // '############5616'
+console.log(maskify('1')); // '1'
+console.log(maskify('11111')); // '#1111'
